2b8b670e18831169abab85ec9e241e6b76283502
Pooria Rezaee <pooriarezaee.dev@gmail.com>
2026-07-11 21:07:07 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const content = document.querySelectorAll(".content")
+const item = document.querySelectorAll(".head")
+console.log(item);
+
+console.log(content);
+
+
+content.forEach((val) => {
+
+    val.setAttribute("data-hight", val.clientHeight)
+    val.setAttribute("data-status", "off")
+    val.style.height = 0
+
+    item.forEach((val2, i) => {
+
+        val2.addEventListener("click", () => {
+
+            content.forEach((para, index) => {
+
+                if (i != index) {
+                    para.style.height = 0
+                    para.setAttribute("data-status", "off")
+                }
+
+            })
+
+            if (val2.nextElementSibling.getAttribute("data-status") == "off") {
+
+                val2.nextElementSibling.style.height = val2.nextElementSibling.getAttribute("data-hight") + "px"
+                val2.nextElementSibling.setAttribute("data-status", "on")
+
+            } else {
+                val2.nextElementSibling.style.height = 0
+                val2.nextElementSibling.setAttribute("data-status", "off")
+            }
+
+
+        })
+
+    })
+
+
+
+})
